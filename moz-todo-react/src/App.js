57ba4fbab261e8605df441bacd70ca5481c00852
andrewrubinstein <andrew.rubinstein93@gmail.com>
2023-04-12 18:27:00 -0400

    Add filtering functionality to filter controls

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -3,10 +3,24 @@ import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import Todo from "./components/Todo";
 let id_index = 100;
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState('All');
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
   const addTask = (name) =>
   {
     const new_id = `todo-${id_index++}`;
@@ -31,7 +45,7 @@ function App(props) {
     setTasks(tasks.filter((el) => el.id !== id))
   }
   const list_heading = `${tasks.length} ${tasks.length !== 1 ? 'tasks' : 'task'} remaining`;
-  const listItems = tasks.map(el => <Todo 
+  const listItems = tasks.filter(FILTER_MAP[filter]).map(el => <Todo 
       name={el.name} 
       completed={el.completed} 
       key={el.id} 
@@ -40,15 +54,12 @@ function App(props) {
       deleteTask={deleteTask}
       editTask={editTask}
     />);
-  
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton name="all"/>
-        <FilterButton name="active"/>
-        <FilterButton name="completed"/>
+        {filterList}
       </div>
       <h2 id="list-heading">{list_heading}</h2>
       <ul
